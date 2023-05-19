0a69ee7fc2ef2c0f1c9cd2a21511e13da13accd3
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-05-19 00:17:49 -0300

    adicionado retorno de token apos login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import { compareSync } from 'bcrypt';
 import { Usuario } from 'src/usuario/entities/usuario.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsuarioService) {}
+  constructor(
+    private readonly userService: UsuarioService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user) {
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 
   async validateUser(email: string, senha: string) {
     let user: Usuario;
